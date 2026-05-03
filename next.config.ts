5bf0119529e2d6a493971d1265fa5897b1a3a0d9
jerry604 <jeremiahomolewa56@gmail.com>
2026-05-03 22:56:09 +0000

    docs: rewrite README for new stack; allow YouTube image hosts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
-      {
-        hostname: "cdn.sanity.io",
-        protocol: "https",
-      },
-      {
-        hostname: "img.clerk.com",
-        protocol: "https",
-      },
+      { protocol: "https", hostname: "img.clerk.com" },
+      { protocol: "https", hostname: "img.youtube.com" },
+      { protocol: "https", hostname: "i.ytimg.com" },
     ],
   },
 };
